60b8493d7b544251c32e9847d25f91bba455957c
yolialisproxy <160247077+yolialisproxy@users.noreply.github.com>
2025-08-17 17:39:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const { createApp } = require('@waline/server');
 
 const app = express();
-app.use(express.json({ limit: '1mb' })); // 确保解析JSON
+app.use(express.json({ limit: '1mb' })); // 解析JSON请求，设置1MB限制
 app.use((req, res, next) => {
-  console.log('Request Body:', req.body); // 调试Body
+  console.log('Request Body:', req.body); // 调试：打印请求Body
   next();
 });
 
